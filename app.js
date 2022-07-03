ef6cdd19b8bb4cb442a6575c4cfd1513091b1e20
Tasfia <tasfia971@gmail.com>
2022-07-03 19:19:22 -0400

    fifth commit

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const squares = 81;
 //array for submissions
 const submissions = [];
 
-// create the squares we want to put in the puzzle
 
+// create the squares we want to put in the puzzle
 for (let i = 0; i < squares; i++) {
     //create an input each time you loop
     const inputElement = document.createElement('input');
@@ -23,7 +23,6 @@ for (let i = 0; i < squares; i++) {
     puzzleBoard.appendChild(inputElement);
 
     // get all the values we can get in the input and then change them into an array
-
     const joinValues = () => {
         
         const inputs = document.querySelectorAll('input');
@@ -33,7 +32,7 @@ for (let i = 0; i < squares; i++) {
         input.array.forEach(input => {
 
         //.push --> adds one or more elements to the end of an array and returns the new length of the array
-           
+
             if(input.value) {
                 submissions.push(input.value);
             }
@@ -49,5 +48,31 @@ for (let i = 0; i < squares; i++) {
     console.log(submissions)
 
 }//for loop
+
+
+//calling the api
+//https://rapidapi.com/sosier/api/solve-sudoku
+const solve = () => {
+    const axios = require("axios");
+
+    const options = {
+    method: 'POST',
+    url: 'https://solve-sudoku.p.rapidapi.com/',
+    headers: {
+        'content-type': 'application/json',
+        'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+        'X-RapidAPI-Host': 'solve-sudoku.p.rapidapi.com'
+    },
+    data: '{"puzzle":"2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3"}'
+    };
+
+    axios.request(options).then(function (response) {
+        console.log(response.data);
+    }).catch(function (error) {
+        console.error(error);
+    });
+}
+
+
 
 solveButton.addEventListener('click', joinValues)
