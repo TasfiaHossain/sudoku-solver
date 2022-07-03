d8cf7419c574674065c58029124e13acbe78bfcf
Tasfia <tasfia971@gmail.com>
2022-07-03 18:43:27 -0400

    third commit

--- app.js
+++ app.js
@@ -12,11 +12,12 @@ const squares = 81
 for (let i = 0; i < squares; i++) {
     //create an input each time you loop
     const inputElement = document.createElement('input')
-    inputElement.setAttribute('type', 'number') //only allows number in this input 
+    
+    //only allows number in this input - create max and min
+    inputElement.setAttribute('type', 'number') 
     inputElement.setAttribute('min', '0')
     inputElement.setAttribute('max', '9')
 
     puzzleBoard.appendChild(inputElement)
-
 }
 
