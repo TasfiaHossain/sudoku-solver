fb443d43b8c1c859972ba2edb91e35ca52e9cafd
Tasfia <tasfia971@gmail.com>
2022-07-03 22:52:30 -0400

    7th commit

--- app.js
+++ app.js
@@ -21,45 +21,53 @@ for (let i = 0; i < squares; i++) {
     inputElement.setAttribute('max', '9');
 
     puzzleBoard.appendChild(inputElement);
+}
 
-    // get all the values we can get in the input and then change them into an array
-    const joinValues = () => {
+// get all the values we can get in the input and then change them into an array
+const joinValues = () => {
+    
+    const inputs = document.querySelectorAll('input');
+
+    // for each input --> get the input and if a value exists in the input, put it in an array 
+
+    inputs.forEach(input => {
+
+    //.push --> adds one or more elements to the end of an array and returns the new length of the array
+
+        if(input.value) {
+            submissions.push(input.value);
+        }
         
-        const inputs = document.querySelectorAll('input');
-
-        // for each input --> get the input and if a value exists in the input, put it in an array 
-
-        input.array.forEach(input => {
-
-        //.push --> adds one or more elements to the end of an array and returns the new length of the array
-
-            if(input.value) {
-                submissions.push(input.value);
-            }
-           
-            else {
-                submissions.push('.'); // '.' symbolizes an empty square 
-            }
-            
-        }); //input.array.forEach
-
-    }//joinValues
+        else {
+            submissions.push('.'); // '.' symbolizes an empty square 
+        }
+        
+    }); //input.array.forEach
 
     console.log(submissions)
 
-}//for loop
+}//joinValues
+
+
 
 const solve = () => {
 
+    joinValues();
+    const data = submissions.join('')
+    console.log('data', data)
+   
     const options = {
-    method: 'POST',
-    url: 'https://solve-sudoku.p.rapidapi.com/',
-    headers: {
-        'content-type': 'application/json',
-        'X-RapidAPI-Key': '2ab7a712a0msh8f8d6ec16910a13p1c1f5fjsne62f1f4c9e78',
-        'X-RapidAPI-Host': 'solve-sudoku.p.rapidapi.com'
-    },
-    data: '{"puzzle":"2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3"}'
+        method: 'POST',
+        url: 'https://solve-sudoku.p.rapidapi.com/',
+        headers: {
+            'content-type': 'application/json',
+            'X-RapidAPI-Key': '2ab7a712a0msh8f8d6ec16910a13p1c1f5fjsne62f1f4c9e78',
+            'X-RapidAPI-Host': 'solve-sudoku.p.rapidapi.com'
+        },
+        
+        data: {
+            puzzle: data
+        }
     };
 
     axios.request(options).then(function (response) {
